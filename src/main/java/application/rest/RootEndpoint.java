d95845c1d22993bda7c100b6648237bba09173de
Steffi Das <sdstefficrazy93@gmail.com>
2021-09-08 14:19:03 +0530

    adding the controller mapping

--- src/main/java/application/rest/RootEndpoint.java
+++ src/main/java/application/rest/RootEndpoint.java
@@ -17,7 +17,7 @@ public class RootEndpoint {
   @Produces(MediaType.APPLICATION_JSON)
   public Response listResources(@Context UriInfo uriInfo) {
     String healthURL = (uriInfo.getAbsolutePath() + "/health").replaceAll("(?<!http:)\\/\\/", "/");
-    String exampleURL = (uriInfo.getAbsolutePath() + "/v1/example").replaceAll("(?<!http:)\\/\\/", "/");
+    String exampleURL = (uriInfo.getAbsolutePath() + "/myapp/serviceA").replaceAll("(?<!http:)\\/\\/", "/");
     return Response.ok("{\"health\":\"" + healthURL + "\",\"example\":\"" + exampleURL + "\"}").build();
   }
 
